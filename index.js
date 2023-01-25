26cb4934b6133ae2b08d1354aa48420670361164
Victor Agese <101999701+AgeseVictor@users.noreply.github.com>
2023-01-25 20:09:39 +0100

    fav change

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const xhr = new XMLHttpRequest();
-let url = "";
+let url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist";
 xhr.open("GET", url);
 xhr.onload = () => {
   if (xhr.status === 200) {
