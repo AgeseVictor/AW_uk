36ebb111cd529b864a3408b333f37323a7c430ba
Victor Agese <101999701+AgeseVictor@users.noreply.github.com>
2023-01-26 01:51:47 +0100

    it did something

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const xhr = new XMLHttpRequest();
 let url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist";
-xhr.open("GET", url);
+xhr.open("GET", url , true);
 xhr.onload = () => {
   if (xhr.status === 200) {
     console.log(xhr.response);
